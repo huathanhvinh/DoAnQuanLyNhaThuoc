a55e921f4fee806f4b160e157b760060c7871509
quoc2203 <68424392+quoc2203@users.noreply.github.com>
2021-11-12 20:51:32 +0700

    Add files via upload

---
+++ DoAnQuanLyNhaThuoc-ThaiQuoc/app/src/main/java/com/example/doanquanlynhathuoc/Chung_DangNhapHeThong.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlynhathuoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chung_DangNhapHeThong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chung_dang_nhap_he_thong);
+    }
+}
---
+++ DoAnQuanLyNhaThuoc-ThaiQuoc/app/src/main/java/com/example/doanquanlynhathuoc/Chung_QuyDinhNhaThuoc.java
@@ -0,0 +1,14 @@
+package com.example.doanquanlynhathuoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chung_QuyDinhNhaThuoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chung_quy_dinh_nha_thuoc);
+    }
+}
