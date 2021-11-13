ca8da2359d0ec8c7b3ab7041456221e3b1833869
quoc <thaiquoc188@gmail.com>
2021-11-13 23:38:33 +0700

    thiet ke giao dien 13/11

---
+++ app/src/main/java/com/example/doanquanlynhathuoc/AD_MainMenu.java
@@ -0,0 +1,23 @@
+package com.example.doanquanlynhathuoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class AD_MainMenu extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuadmin,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_main_menu);
+        //hiển thị menu
+
+    }
+}
