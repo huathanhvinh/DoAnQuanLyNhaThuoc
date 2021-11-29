00a67fb980894dfcdd6d9d063e717da57e93382d
huathanhvinh <vinh.huathanh01@gmail.com>
2021-11-29 23:45:56 +0700

    Thiết kế giao diện Tuần 3 - lần 1

--- app/src/main/java/com/example/doanquanlynhathuoc/AD_BaoCaoTonThuoc.java
+++ app/src/main/java/com/example/doanquanlynhathuoc/AD_BaoCaoTonThuoc.java
@@ -2,15 +2,13 @@ package com.example.doanquanlynhathuoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AD_BaoCaoTonThuoc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),AD_MainMenu.class));
+        setContentView(R.layout.activity_ad_bao_cao_ton_thuoc);
     }
 }
--- app/src/main/java/com/example/doanquanlynhathuoc/NVK_DanhSachThuoc.java
+++ app/src/main/java/com/example/doanquanlynhathuoc/NVK_DanhSachThuoc.java
@@ -2,15 +2,13 @@ package com.example.doanquanlynhathuoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NVK_DanhSachThuoc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),AD_MainMenu.class));
+        setContentView(R.layout.activity_nvk_danh_sach_thuoc);
     }
 }
