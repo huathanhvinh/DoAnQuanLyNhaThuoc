939b073d6b2cbbae4afaae92f4c29077059863e5
huathanhvinh <vinh.huathanh01@gmail.com>
2021-11-30 21:00:06 +0700

    Code chức Tuần 3 - lần 3

--- app/src/main/java/com/example/doanquanlynhathuoc/AD_MainMenu.java
+++ app/src/main/java/com/example/doanquanlynhathuoc/AD_MainMenu.java
@@ -7,17 +7,16 @@ import android.view.Menu;
 
 public class AD_MainMenu extends AppCompatActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuadmin,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menuadmin,menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ad_main_menu);
-        //hiển thị menu
+
 
     }
 }
--- app/src/main/java/com/example/doanquanlynhathuoc/MainActivity.java
+++ app/src/main/java/com/example/doanquanlynhathuoc/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.doanquanlynhathuoc.Class.Account;
+import com.example.doanquanlynhathuoc.Config.StaticConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),NVBH_ThemHoaDon.class));
+        startActivity(new Intent(getApplicationContext(), Chung_DangNhapHeThong.class));
     }
 }
