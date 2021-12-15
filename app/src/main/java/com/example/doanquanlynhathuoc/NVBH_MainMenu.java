4605397eec1c6b990857256e274026b9e056de1b
quoc <thaiquoc188@gmail.com>
2021-12-15 23:00:51 +0700

    update code

--- app/src/main/java/com/example/doanquanlynhathuoc/NVBH_MainMenu.java
+++ app/src/main/java/com/example/doanquanlynhathuoc/NVBH_MainMenu.java
@@ -1,14 +1,21 @@
 package com.example.doanquanlynhathuoc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.doanquanlynhathuoc.Config.StaticConfig;
 
 public class NVBH_MainMenu extends AppCompatActivity {
-Button btnDoiMatKhau,btnThemHoaDon;;
+    TextView tvTaiKhoan;
+    Button btnLapHoaDon,btnThemKH,btnTraCuuHD,btnXemDSHD,btnDanhSachKH,btnDoiMatKhau;
+    Button btnDangXuat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,43 @@ Button btnDoiMatKhau,btnThemHoaDon;;
     }
 
     private void setEvent() {
+        //lấy tài khoản
+        tvTaiKhoan.setText("Xin chào: "+ StaticConfig.taiKhoan);
+        //nút lập hóa đơn
+        btnLapHoaDon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),NVBH_ThemHoaDon.class));
+            }
+        });
+        //nút thêm khách hàng
+        btnThemKH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),NVBH_ThemKhachHang.class));
+            }
+        });
+        //Tra cứu hóa đơn
+        btnTraCuuHD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),NVBH_TraCuuHoaDon.class));
+            }
+        });
+        //nút xem danh sách hóa đơn
+        btnXemDSHD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),NVBH_DanhSachHoaDon.class));
+            }
+        });
+        //danh sách khách hàng
+        btnDanhSachKH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),NVBH_DanhSachKhachHang.class));
+            }
+        });
         //nút đổi mật khẩu
         btnDoiMatKhau.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,17 +69,40 @@ Button btnDoiMatKhau,btnThemHoaDon;;
                 startActivity(new Intent(getApplicationContext(),Chung_DoiMatKhau.class));
             }
         });
-        //nút thêm hóa đơn
-        btnThemHoaDon.setOnClickListener(new View.OnClickListener() {
+        //nút đăng xuất
+        btnDangXuat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),NVBH_ThemHoaDon.class));
+                AlertDialog.Builder builder = new AlertDialog.Builder(NVBH_MainMenu.this);
+                builder.setTitle("Thông Báo");
+                builder.setMessage("bạn có muốn quay lại màn hình đăng nhập không ?");
+                builder.setPositiveButton("có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getApplicationContext(),Chung_DangNhapHeThong.class));
+                    }
+                });
+                builder.setNegativeButton("không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
             }
         });
+
+
     }
 
     private void setControl() {
-        btnDoiMatKhau = findViewById(R.id.btnDoiMatKhauNVBH);
-        btnThemHoaDon = findViewById(R.id.btnThemHoaDon);
+        tvTaiKhoan = findViewById(R.id.tvTaiKhoan);
+        btnLapHoaDon = findViewById(R.id.btnLapHoaDon);
+        btnThemKH = findViewById(R.id.btnThemKhachHang);
+        btnTraCuuHD = findViewById(R.id.btnTraCuuHoaDon);
+        btnXemDSHD = findViewById(R.id.btnDanhSachHoaDon);
+        btnDanhSachKH = findViewById(R.id.btnDanhSachKhachHang);
+        btnDoiMatKhau = findViewById(R.id.btnDoiMatKhau);
+        btnDangXuat = findViewById(R.id.btnDangXuat);
     }
 }
